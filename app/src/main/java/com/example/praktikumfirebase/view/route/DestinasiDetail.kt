bbd2e2053d128a2e55337e8fa4832f88650790fc
halimbagusalhakim <halsat132@gmail.com>
2026-01-02 17:38:14 +0700

    mendefinisikan DestinasiDetail sebagai destinasi navigasi untuk halaman detail siswa, dengan rute "detail_siswa" dan judul detail_siswa. Properti itemIdArg dan routeWithArgs memungkinkan pengiriman ID siswa sebagai argumen pada navigasi ke halaman detail.

---
+++ app/src/main/java/com/example/praktikumfirebase/view/route/DestinasiDetail.kt
@@ -0,0 +1,10 @@
+package com.example.praktikumfirebase.view.route
+
+import com.example.praktikumfirebase.R
+
+object DestinasiDetail : DestinasiNavigasi {
+    override val route = "detail_siswa"
+    override val titleRes = R.string.detail_siswa
+    const val itemIdArg = "idSiswa"
+    val routeWithArgs = "$route/{$itemIdArg}"
+}
