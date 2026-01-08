b085a7888f4ce5bbeb502096814f6e5acd7d985e
halimbagusalhakim <halsat132@gmail.com>
2026-01-08 12:08:43 +0700

    update gradle

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,6 +61,8 @@ dependencies {
     //viewmodel
     implementation(libs.lifecycle.viewmodel.compose)
     implementation(libs.lifecycle.runtime.compose)
+    implementation(libs.androidx.lifecycle.viewmodel.savedstate)
+
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
